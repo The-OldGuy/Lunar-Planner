102843c498929657e586695c018adaa5ce78c798
The-OldGuy <oldguy1@togsurvival.com>
2026-08-22 15:37:24 -0400

    Add notes page to display user notes

---
+++ LunarPlanner/src/routes/notes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let data;
+    const notes = data.notes;
+</script>
+
+<h1>Notes</h1>
+
+{#if notes.length === 0}
+    <p>No notes yet.</p>
+{:else}
+    <ul>
+        {#each notes as note}
+            <li>{note.title}</li>
+        {/each}
+    </ul>
+{/if}
