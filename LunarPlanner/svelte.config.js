5090df70b6e421ef4035dc7685b834f41d35d453
The-OldGuy <oldguy1@togsurvival.com>
2026-08-22 17:15:11 -0400

    Remove alias configuration in svelte.config.js
    Removed alias configuration from SvelteKit setup.

--- LunarPlanner/svelte.config.js
+++ LunarPlanner/svelte.config.js
@@ -1,13 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
-    alias: {
-      $components: 'src/lib/components',
-      $utils: 'src/lib/utils'
-    }
+    adapter: adapter()
   }
 };
 
 export default config;
+
