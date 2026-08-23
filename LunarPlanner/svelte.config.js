bc67441f20cb07a2236a6b211291ec0c13a38d54
The-OldGuy <oldguy1@togsurvival.com>
2026-08-23 14:56:43 -0400

    Add path aliasing for components and lib

--- LunarPlanner/svelte.config.js
+++ LunarPlanner/svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
+import path from 'path';
 
 const config = {
   preprocess: sveltePreprocess(),
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    alias: {
+      $components: path.resolve('src/lib/components'),
+      $lib: path.resolve('src/lib')
+    }
   }
 };
 
